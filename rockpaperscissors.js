09fba463b6e6efbd1f6538a5f773020dffde7fb2
Matthias Hochmuth <deveolpment@matthias-hochmuth.de>
2024-09-30 09:11:45 +0200

    computer choice

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,2 +1,33 @@
 
-let playerChoice = parseInt(prompt("Please select (R)ock (P)aper or (S)cissors: "));
+//checkInput(userInput());
+
+function getComputerChoice(){
+    let computerChoice = "";
+    switch(getRandomInt(3)){
+        case 0:
+            computerChoice = "Rock";
+            break;
+        case 1:
+            computerChoice = "Paper";
+            break;
+        case 2:
+            computerChoice = "Scissors";
+            break;
+    }
+
+    return computerChoice;
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+console.log("The computers choice: " + getComputerChoice());
+
+function userInput() {
+    let playerChoice = parseInt(prompt("Please select (R)ock, (P)aper or (S)cissors: "));
+}
+
+function checkInput(){
+
+}
