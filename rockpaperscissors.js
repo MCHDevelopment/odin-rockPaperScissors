c75ecc92af2f8965ad1b5aa68ac0fdc6f9700ca1
Matthias Hochmuth <deveolpment@matthias-hochmuth.de>
2024-09-30 09:29:27 +0200

    input validation

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,9 +1,9 @@
 
 //checkInput(userInput());
 
-function getComputerChoice(){
+function getComputerChoice() {
     let computerChoice = "";
-    switch(getRandomInt(3)){
+    switch (getRandomInt(3)) {
         case 0:
             computerChoice = "Rock";
             break;
@@ -29,17 +29,36 @@ userInput();
 function userInput() {
     let playerInput = prompt("Please select (R)ock, (P)aper or (S)cissors: ");
     let playerChoice = checkInput(playerInput);   //second variable to keep the original input for the alert
-    if(playerChoice != "invalid")
-    {
+    if (playerChoice != "invalid") {
         console.log("The players choice: " + playerChoice);
     }
-    else
-    {
+    else {
         alert(playerInput + " is not a valid Value, please choose either Rock, Paper or Scissors")
         playerChoice = userInput();
     }
 }
 
-function checkInput(playerInput){
-    return playerInput;
+function checkInput(originalInput) {
+    let validatedInput = "invalid";
+
+    if (typeof originalInput === "string" && originalInput.length === 0) {
+        alert("The input is empty");
+    } else if (originalInput === null) {
+        console.log("The input is null");
+    } else {
+        let firstChar = originalInput[0].toUpperCase();
+        switch(firstChar){
+            case "R":
+                validatedInput = "Rock";
+                break;
+            case "P":
+                validatedInput = "Paper";
+                break;
+            case "S":
+                validatedInput = "Scissors";
+                break;
+        }
+    }
+
+    return validatedInput;
 }
